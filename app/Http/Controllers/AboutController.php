bead50b2c613d2c4454640ad603c34e5b7f42fb2
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2023-11-17 09:33:57 +0100

    bug fixed on AboutController

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AboutController extends Controller
 {
     
-    $team = [
+    public $team = [
         [
             'prenom' => 'Julie', 
             'nom' => 'Vandenberghe', 
@@ -26,7 +26,7 @@ class AboutController extends Controller
             'fonction' => 'ronronneur', 
             'image' => 'https://images.unsplash.com/photo-1490650034439-fd184c3c86a5?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         ],
-    ]
+    ];
     
     public function index() {
         return view('about', [
