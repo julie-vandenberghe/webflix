813c8e0c25d08af3c9d17d4b968a8ed403cd102e
Julie Vandenberghe <julie.vandenberghe.59@gmail.com>
2023-11-17 13:56:30 +0100

    check the data in the form + errors display (in englsih)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -21,13 +21,18 @@ class CategoryController extends Controller
 
     public function create()
     {
-       
-
         return view('categories/create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        //Vérification des données
+        $request->validate([
+            'name' => 'required|min:3|unique:categories|max:10',
+            //nom = obligatoire, min 3 caractères, pas de doublon, maximum 10 caractères
+        ]);
+        
+        //Insertion en BDD
         $category = new Category();
         $category->name = 'Action'; //Laravel crée lui-même un tableau avec les attributs et leurs valeurs via la class Category et le "use HasFactory;"
         $category->save(); //Fait un INSERT INTO categories en Laravel :o
--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -2,7 +2,13 @@
 
 @section('content') 
 
+    {{--Boucle pour afficher les erreurs. Laravel s'occupe même des messages (mais en anglais par contre) --}}
+    @foreach ($errors->all() as $error)
+    {{ $ error}}
+    @endforeach
+
     <form action="" method="post">
+        @csrf {{-- Anytime you define an HTML form in your application, you should include a hidden CSRF token field in the form so that the CSRF protection middleware can validate the request.--}}
         <input type="text" name="name">
         <button>Sauvegarder</button>
     </form>
